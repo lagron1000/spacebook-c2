4d48c6b4a8eb3b3c0b19ddebcc98335b6392e688
lagron1000 <lagron1000@gmail.com>
2018-04-24 09:00:49 +0300

    modular incomplete

--- main.js
+++ main.js
@@ -32,7 +32,8 @@ var SpacebookApp = function () {
   var createPost = function (text) {
     var post = {
       text: text,
-      id: currentId
+      id: currentId,
+      comments: []
     }
 
     currentId += 1;
@@ -40,19 +41,63 @@ var SpacebookApp = function () {
     posts.push(post);
   }
 
+  var createComment = function (currentPost, text) {
+    var $clickedPost = $(currentPost).closest('.post');
+    var id = $clickedPost.data().id;
+    var post = _findPostById(id);
+    posts[posts.indexOf(post)].comments.push(text)
+    var findId = function(){
+      return id;
+    }
+    console.log(posts[posts.indexOf(post)].comments)
+  }
+
+  var renderComments = function (currentPost) {
+    findId();
+    // var $clickedPost = $(currentPost).closest('.post');
+    // var id = $clickedPost.data().id;
+    var post = _findPostById(id);
+    $(".comDiv").empty();
+    for (let i = 0; i < posts.length; i++) {
+      for (let j = 0; j < posts[i].comments.length; j++) {
+
+        var commentsContainer = '<div class="comDiv"><li>' + posts[i].comments[j] + '</li> </div>';
+        $(currentPost).siblings('#' + id).append(commentsContainer)
+
+      }
+    }
+  }
+
+  //   var renderComments = function () {
+  //       $('.commentsContainer').empty();
+  //       for (let i=0; i<posts.length; i++){
+  //           for (let j=0; j<posts[i].comments.length; j++){
+  //           var com = posts[i].comments[j]
+  //             //   var commentsContainer = '<div class="comments">' +
+  //             //    '<input type="text" class="comment-name">' +
+  //             //    '<button class="btn btn-primary add-comment">Post Comment</button>' + posts[i].comments[j] + '</div>';
+  //           $posts.append('<div class="comments">' +
+  //           '<input type="text" class="comment-name">' +
+  //           '<button class="btn btn-primary add-comment">Post Comment</button>' + posts[i].comments[j] + '</div>')
+  //       }
+  //     }
+  //   }
+
   var renderPosts = function () {
     $posts.empty();
 
     for (var i = 0; i < posts.length; i += 1) {
       var post = posts[i];
 
-      var commentsContainer = '<div class="comments-container">' +
-      '<input type="text" class="comment-name">' +
-      '<button class="btn btn-primary add-comment">Post Comment</button> </div>';
+      var commentsContainer = '<div class="comments">' +
+        '<input type="text" class="comment-name">' +
+        '<button class="btn btn-primary add-comment">Post Comment</button>' +
+        '<ul id="' + post.id + '"></ul> </div>';
 
       $posts.append('<div class="post" data-id=' + post.id + '>'
         + '<a href="#" class="remove">remove</a> ' + '<a href="#" class="show-comments">comments</a> ' + post.text +
         commentsContainer + '</div>');
+        renderComments(post);
     }
   }
 
@@ -68,7 +113,7 @@ var SpacebookApp = function () {
 
   var toggleComments = function (currentPost) {
     var $clickedPost = $(currentPost).closest('.post');
-    $clickedPost.find('.comments-container').toggleClass('show');
+    $clickedPost.find('.comments').toggleClass('show');
   }
 
   return {
@@ -77,10 +122,11 @@ var SpacebookApp = function () {
     removePost: removePost,
 
     // TODO: Implement
-    // createComment: createComment,
+    createComment: createComment,
 
     // TODO: Implement
-    // renderComments: renderComments,
+    renderComments: renderComments,
+    posts: posts,
 
     // TODO: Implement
     // removeComment: removeComment,
@@ -96,7 +142,7 @@ app.renderPosts();
 // Events
 $('.add-post').on('click', function () {
   var text = $('#post-name').val();
-  
+
   app.createPost(text);
   app.renderPosts();
 });
@@ -105,6 +151,12 @@ $('.posts').on('click', '.remove', function () {
   app.removePost(this);
 });
 
-$('.posts').on('click','.show-comments', function () {
+$('.posts').on('click', '.show-comments', function () {
   app.toggleComments(this);
 });
+
+$(".posts").on('click', '.add-comment', function () {
+  var text = $(this).siblings($(".comment-name")).val()
+  app.createComment(this, text);
+  // app.renderComments(this);
+})
